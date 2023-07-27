8bcd69dae2c53514268b4932a938f7c9ca2af17f
Jean-Claude Twagiramungu <jean-claude.twagiramungu@lacapitale.com>
2023-07-27 15:58:16 -0400

    add cross consent - not sync

---
+++ onetrust/token.js
@@ -0,0 +1,26 @@
+  <script language="JavaScript" type="text/javascript"
+          src="https://kjur.github.io/jsrsasign/jsrsasign-latest-all-min.js">
+  </script>
+
+  <script>
+
+const PUBLIC_KEY = 'Q29va2llIEJhbm5lciB1c2luZyBPbmVUcnVzdA==';
+  // Header
+var oHeader = {alg: 'HS256', typ: 'JWT'};
+// Payload
+var oPayload = {};
+var tNow = KJUR.jws.IntDate.get('now');
+var tEnd = KJUR.jws.IntDate.get('now + 1day');
+//oPayload.iss = "http://foo.com";
+oPayload.sub = "jeanclaude.twagiramungu@onetrust.com";
+oPayload.nbf = tNow;
+oPayload.iat = tNow;
+oPayload.exp = tEnd;
+//oPayload.jti = "id123456";
+//oPayload.aud = "http://foo.com/employee";
+// Sign JWT, password=616161
+var sHeader = JSON.stringify(oHeader);
+var sPayload = JSON.stringify(oPayload);
+var sJWT = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, PUBLIC_KEY);
+
+console.log(sJWT);
